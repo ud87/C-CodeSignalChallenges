391509312cf9bf77edd636c38d3eab1123e0430f
unknown <udayarai@hotmail.co.uk>
2025-06-12 22:08:50 +0100

    Problem statement added

--- ReverseStringHalfwayApp/ReverseStringHalfway/Program.cs
+++ ReverseStringHalfwayApp/ReverseStringHalfway/Program.cs
@@ -1,4 +1,33 @@
-﻿using System;
+﻿/*
+ In this task, you are given a string composed of lowercase English 
+alphabet letters ('a' to 'z'). The length of the string will range 
+from 1 to 100 characters. Your challenge is to create a new string 
+resulting from a unique order of character selection from the 
+original string.
+
+You need to develop a C# function, SpecialOrder(string inputString),
+which takes inputString as an argument. The resulting string begins 
+with the last character of the inputString, then selects the 
+second-to-last character, continuing in reverse order until you 
+reach the middle character of the string. Then, start with the 
+first character of the inputString, proceed to the second character,
+and continue in this manner until you reach the middle character.
+
+For example, if the inputString is "abcdefg", the function should 
+return "gfedabc".
+
+Keep in mind the following constraints while creating your function:
+
+
+
+The input string contains only lowercase English letters ('a' to 'z').
+The length of the input string is between 1 and 100, inclusive.
+ 
+ 
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
