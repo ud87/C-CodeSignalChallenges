a74182bca9a654e4ab8470ae6882b43e2abb6ccc
unknown <udayarai@hotmail.co.uk>
2025-06-26 22:33:49 +0100

    Problem statement added

--- PairingElementsFromMiddleToEndsApp/PairingElementsFromMiddleToEnds/Program.cs
+++ PairingElementsFromMiddleToEndsApp/PairingElementsFromMiddleToEnds/Program.cs
@@ -1,4 +1,29 @@
-﻿using System;
+﻿/*
+ You are given an array of n integers, where n can range from 1 to 500, 
+inclusive. Your task is to create a new array in which each element is 
+a tuple, determined by pairing elements from the middle to both ends of 
+the original array.
+
+If the original array has an odd length, pair the middle element with 0. 
+If the original array has an even length, start pairing from the two 
+middle elements. Continue the pairing by alternating elements from the 
+left and the right until all elements have been paired.
+
+After creating the paired elements, return the new array of tuples. 
+Ultimately, your result should be an array of tuples, each of size two, 
+where each element within a tuple, as well as within the array, can range 
+from -1000 to 1000, inclusive.
+
+Note: You are not allowed to use built-in methods that reverse the order 
+of elements, such as the Reverse() method.
+
+For example, if the input is numbers = [1, 2, 3, 4, 5], the output should 
+be [(3, 0), (2, 4), (1, 5)]. Similarly, if the input is numbers = [1, 2, 3, 4], 
+the output should be [(2, 3), (1, 4)].
+ */
+
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -55,7 +80,7 @@ namespace PairingElementsFromMiddleToEnds
             }
 
             Console.WriteLine(string.Join(", ", tupleResult));
-            return new List<(int, int)>();
+            return tupleResult;
         }
     }
 }
