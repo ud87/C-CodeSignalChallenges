ffd073f51776a799ec93d70d92c1604ad867f638
unknown <udayarai@hotmail.co.uk>
2025-06-29 13:13:47 +0100

    Initial commit

--- UnusualArrayTraversalApp/UnusualArrayTraversal/Program.cs
+++ UnusualArrayTraversalApp/UnusualArrayTraversal/Program.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+You are provided with an array of n integers, where n ranges from 
+1 to 501 and is always an odd number. The goal is to return a new array constructed 
+by traversing the initial array in a specific order, outlined as follows:
+
+Begin with the middle element of the array.
+For each subsequent pair of elements, alternate between taking two elements from the 
+left and two elements from the right, relative to the middle.
+If fewer than two elements remain on either side, include all the remaining 
+elements from that side.Continue this process until all elements of the array have 
+been traversed.
+For example, for array = [1, 2, 3, 4, 5, 6, 7], your function should 
+return [4, 2, 3, 5, 6, 1, 7]. And for array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 
+your function should return [6, 4, 5, 7, 8, 2, 3, 9, 10, 1, 11].
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +26,7 @@ namespace UnusualArrayTraversal
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int> { 1, 2, 3, 4, 5, 6, 7};
+            List<int> numbers = new List<int> { 3, 1, 2, 4, 5, 1, 5 };
             Solution.UnusualTraversal(numbers);
             Console.ReadLine();
         }
@@ -25,8 +41,8 @@ namespace UnusualArrayTraversal
 
             int middle = length / 2;
 
-            int left = middle - 2;
-            int right = middle + 2;
+            int left = middle - 1;
+            int right = middle + 1;
 
             List<int> newArray = new List<int> { }; //new empty array
             newArray.Add(array[middle]);
@@ -34,35 +50,37 @@ namespace UnusualArrayTraversal
             while (left >= 0 || right<length)
             {
                 //add two element from left
-                newArray.Add(array[left]);
-                newArray.Add(array[left + 1]);
+                if (left >= 0)
+                {
+                    if (left >= 0 && left - 1 >= 0)
+                    {
+                        newArray.Add(array[left - 1]);
+                        newArray.Add(array[left]);
 
-                //add two element from right
-                newArray.Add(array[right - 1]);
-                newArray.Add(array[right]);
+                        Console.WriteLine($"Left: {left}");
+                    }
+                    else newArray.Add(array[left]);
+                    left -= 2;
+                }
+                
+                if (right < length) //add two element from right
+                {
+                    if (right < length && right + 1 < length)
+                    {
+                        newArray.Add(array[right]);
+                        newArray.Add(array[right + 1]);
+                        Console.WriteLine($"Right: {right}");
+                    }
+                    else newArray.Add(array[right]);
+                    right += 2;
+                }
 
-                left -= 2;
-                right += 2;
-
+                
             }
-            
-
-            //if left is less than 0 or right is equal/greater than index length 
-            if (left < 0)
-            {
-                //final odd ones will always be 0 index for left and length - 1 index for right
-                newArray.Add(array[0]);
-            }
-
-            if (right >= length)
-            {
-                newArray.Add(array[length - 1]);
-            }
 
 
-            //less than two elements on left or right
-            //include all elements
-                Console.WriteLine(string.Join(", ", newArray));
+            Console.WriteLine("Hello");
+            Console.WriteLine(string.Join(", ", newArray));
 
             return newArray;
         }
