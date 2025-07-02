e15ce977b2e25afd98c2d7e54ca16afdf861226d
unknown <udayarai@hotmail.co.uk>
2025-07-02 22:04:00 +0100

    Logic added

--- RunLengthEncodingInCSharpApp/RunLengthEncodingInCSharp/Program.cs
+++ RunLengthEncodingInCSharpApp/RunLengthEncodingInCSharp/Program.cs
@@ -10,6 +10,62 @@ namespace RunLengthEncodingInCSharp
     {
         static void Main(string[] args)
         {
+            Solution.EncodeRle("aaa@@bb!!c#d**e");
+            Console.ReadLine();
+        } 
+    }
+
+    public class Solution
+    {
+        public static string EncodeRle(string s)
+        {
+            // TODO: implement
+
+            string result = "";
+            int count = 0;
+            char? group = null;
+            string letter;
+            string number;
+
+            //foreach loop
+            foreach (char ch in s)
+            {
+                //each character is digit or a alphabet
+                if (Char.IsLetter(ch))
+                {
+                    // if group empty, then group = character
+                    if (group == null)
+                    {
+                        group = ch;
+                        count++;
+                    }
+                    // if group is not empty and char == group, count ++
+                    else if (group != null && group == ch)
+                    {
+                        count++; //increase count increment by one
+                    }
+                    // if group is not equal to ch then group = char, count = 0
+                    else if (group != ch)
+                    {
+                        letter = group.ToString();
+                        number = count.ToString();
+                        result += letter + number;
+
+                        //reset group with ch
+                        group = ch;
+                        count = 1;
+                    }
+                }
+            }
+
+            //process the last letter and number
+            letter = group.ToString();
+            number = count.ToString();
+            result += letter + number;
+
+            Console.WriteLine(result);
+
+            return result;
         }
     }
 }
