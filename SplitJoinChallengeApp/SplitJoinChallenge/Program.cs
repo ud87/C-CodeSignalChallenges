075184d69924feeb156a2e7d913bc6e2dcd18c29
unknown <udayarai@hotmail.co.uk>
2025-07-12 10:11:00 +0100

    Initial commit

---
+++ SplitJoinChallengeApp/SplitJoinChallenge/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SplitJoinChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string astronautsData = "Buzz Aldrin, 1930;Yuri Gagarin, 1934;Valentina Tereshkova, 1937";
+
+            // Splitting the string into a list of astronaut info and stripping any whitespace
+            string[] astronautsList = astronautsData.Split(';');
+            string[] cleanedAstronauts = new string[astronautsList.Length];
+
+            for (int i = 0; i < astronautsList.Length; i++)
+            {
+                string[] parts = astronautsList[i].Split(',');
+                //cleanedAstronauts[i] = parts[0].Trim() + " " + parts[1].Trim(); // Modify this line to use the Join() method
+                string name = parts[0].Trim();
+                string year = parts[1].Trim();
+                cleanedAstronauts[i] = string.Join(" ", name, year);
+            }
+
+            Console.WriteLine(string.Join(", ", cleanedAstronauts)); // Output: "Buzz Aldrin 1930, Yuri Gagarin 1934, Valentina Tereshkova 1937"
+            Console.ReadLine();
+        }
+    }
+    
+}
