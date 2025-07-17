6469296460dd70fb52eac169f17bc54949fd7ec2
Udaya <udayarai@hotmail.co.uk>
2025-07-17 21:45:00 +0100

    Logic amended to only add c if it is not the last character

--- StringRotationSplitApp/StringRotationSplit/Program.cs
+++ StringRotationSplitApp/StringRotationSplit/Program.cs
@@ -1,4 +1,27 @@
-﻿using System;
+﻿/*
+ You are given a string of n characters, with n varying from 1 to 1000, inclusive. Your task is to write
+a C# function that takes this string as input, applies the following operations, and finally returns the
+resulting string.
+
+Split the given string into individual words, using a space as the separator.
+Convert each word into a list of its constituent characters, and shift each list once to the right (with
+the last element moving to the first position).
+
+After the rotations, reassemble each word from its list of characters.
+Join all the words into a single string, separating adjacent words with a single space.
+Return this final string as the function's output.
+The constraints for the problem are as follows:
+
+The input string will neither start nor end with a space, nor will it have multiple consecutive spaces.
+Each word will contain only alphabets and digits, and its length will range from 1 to 10.
+Your program should output a single string with the words rotated by their lengths while preserving 
+their original order.
+
+As an illustration, consider the input string "abc 123 def". Applying the stated operations results in 
+the output "cab 312 fde".
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -31,13 +54,16 @@ public class Solution
             //assign last character to a variable and then iterate through the word
             string modifiedWord = word[word.Length - 1].ToString();
 
+            int index = 0;
+
             // iterate through the word except the last character
             foreach (char c in word)
             {
-                if (c != word[word.Length - 1] && i != word.Length)
+                if (index != word.Length - 1) //only add c if it is not the last character
                 {
                     modifiedWord +=  c; 
                 }
+                index++;
             }
 
             resultArray[i] = modifiedWord;
@@ -52,6 +78,6 @@ public class Solution
         string result = string.Join(" ", resultArray);
         Console.WriteLine(result);
 
-        return null;
+        return result;
     }
 }
