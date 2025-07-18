5a005e7adb1666a670e9930d9495ba707378f19a
Udaya <udayarai@hotmail.co.uk>
2025-07-18 10:58:45 +0100

    Initial commit

---
+++ StringSplitReplaceOppositeCharApp/StringSplitReplaceOppositeChar/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/* Create a program that takes in string splits it into words, converts the words into opposite characters
+ and then returns the last word followed by all the other words. 
+
+e.g. "Hello World" should return Dliow Svool
+ 
+ */
+
+
+namespace StringSplitReplaceOppositeChar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution.TransformString("Hello World");
+        }
+    }
+
+    public class Solution
+    {
+        public static string TransformString(string inputStr)
+        {
+            // TODO: implement the string transformation function
+            string[] words = inputStr.Split(' ');
+
+            string[] modifiedWords = new string[words.Length];
+
+            int modifiedWordsIndex = 0;
+
+
+            foreach (string word in words)
+            {
+                // Initialize an empty string to hold the modified word
+                string modifiedWord = "";
+
+                foreach (char c in word)
+                {
+                    if (Char.IsUpper(c))
+                    {
+                        //calculate distance between c and 'A'
+                        int distance = c - 'A';
+                        int oppositeChar = 'Z' - distance;
+                        modifiedWord += (char)oppositeChar;
+
+                    }
+                    else
+                    {
+                        //calculate distance between c and 'a'
+                        int distance = c - 'a';
+                        int oppositeChar = 'z' - distance;
+                        //Console.WriteLine((char)oppositeChar);
+                        modifiedWord += (char)oppositeChar;
+
+                    }
+                }
+
+                // Add the modified word to the modifiedWords array
+                modifiedWords[modifiedWordsIndex] = modifiedWord;
+                modifiedWordsIndex++;
+            }
+
+            //extract last word from modifiedWords array
+            string lastWord = modifiedWords.Last();
+            // Remove the last word from the modifiedWords array
+            string allExceptLast = string.Join(" ", modifiedWords.Take(modifiedWords.Length - 1));
+
+            string result;
+
+            // If there are no other words, just return the last word
+            if (string.IsNullOrEmpty(allExceptLast))
+            { 
+                result = lastWord;
+            }
+            else
+            {
+                result = $"{lastWord} {allExceptLast}";
+            }
+            
+            Console.WriteLine(result);
+
+            return result;
+        }
+    }
+}
---
+++ StringSplitReplaceOppositeCharApp/StringSplitReplaceOppositeChar/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("StringSplitReplaceOppositeChar")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("StringSplitReplaceOppositeChar")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a34da40c-231d-4b71-bde5-7cc98349448a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
