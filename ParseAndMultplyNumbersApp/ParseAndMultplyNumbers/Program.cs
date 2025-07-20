b86e426669c5b879783a86786b8c3d94c1a1c104
Udaya <udayarai@hotmail.co.uk>
2025-07-20 09:16:47 +0100

    Initial commit

---
+++ ParseAndMultplyNumbersApp/ParseAndMultplyNumbers/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseAndMultplyNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ParseAndMultiplyNumbers("I have2apples and5oranges");
+        }
+
+        static int ParseAndMultiplyNumbers(string input)
+        {
+            string[] strings = input.Split(' ');
+
+            List<int> numbers = new List<int>();
+
+            foreach (string str in strings)
+            {
+                foreach (char c in str)
+                {
+                    if (int.TryParse(c.ToString(), out int numericValue))
+                    {
+                        numbers.Add(numericValue);
+                    }
+                }
+            }
+
+            int product = numbers.Aggregate(1, (current, number) => current * number);
+
+            Console.WriteLine($"The product of the numbers is: {product}");
+            return product;
+
+
+        }
+    }
+}
