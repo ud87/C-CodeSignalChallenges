fe1a4ac4a9df2d7d16a3c85ac4577415457f7048
unknown <udayarai@hotmail.co.uk>
2025-07-27 20:23:16 +0100

    Initial commit

---
+++ AddSecondsToTimes/AddSecondsToTimes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddSecondsToTimes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] timePoints = new string[] { "01:02:03", "02:04:55" };
+           AddSecondsToTimes(timePoints, 366);
+            Console.ReadLine();
+        }
+
+        public static string[] AddSecondsToTimes(string[] timePoints, int addedSeconds)
+        {
+            // TODO: implement the function
+            foreach (string time in timePoints)
+            {
+                Console.WriteLine(time);
+                int hours = int.Parse
+            }
+            return new string[0]; // Placeholder
+        }
+    }
+}
